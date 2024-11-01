011220c981a376cacca807459c67bd6acddb297c
Salmaan M <salmaansalmaan114@gmail.com>
2024-11-01 17:41:09 +0530

    First commit

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,13 @@
+import styles from './ProjectsStyles.module.css'
+function Projects() {
+  return (
+    <section id='projects' className={StyleSheet.container}>
+      <h1  className='sectionTitle'>Projects</h1>
+      <div className={styles.projectsContainer}>
+        <a href=''
+      </div>
+    </section>
+  )
+}
+
+export default Projects
