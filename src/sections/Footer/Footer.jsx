27143ceb9a2d34bceb02a99f17c225dd46917be1
Salmaan M <salmaansalmaan114@gmail.com>
2025-08-20 17:24:46 +0530

    Update Footer.jsx
    change

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './FooterStyles.module.css'
 function Footer() {
   return (
    <section id='footer' className={styles.container}>
-     <p>&copy; 2024 Salmaan M. <br/>
+     <p>&copy; 2024 Salmaan M.<br/>
      All rights reserved.</p>
    </section>
   );
